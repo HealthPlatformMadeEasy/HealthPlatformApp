afb439c941174eb866018fcc40e7464ca933ebda
Maximiliano Barrientos <elbodysathba@gmail.com>
2023-06-27 14:14:47 +0200

    in the middle it need to add liquibase or another alternative for migrations

--- Server/src/main/java/com/api/server/entities/User.java
+++ Server/src/main/java/com/api/server/entities/User.java
@@ -1,18 +1,20 @@
 package com.api.server.entities;
 
-//import jakarta.persistence.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
+
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-//@Entity
+@Entity
 public class User {
+    @Id
     private int id;
     private String name;
     private String email;
     private String password;
-
 }
