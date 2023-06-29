270483e2e85a42c26ddd9c3a0eb20f1c8ab17d78
farhanak <farhana.khashiya@gmail.com>
2023-06-29 11:03:19 +0200

    test commit

--- Server/src/main/java/com/api/server/repositories/UserRepository.java
+++ Server/src/main/java/com/api/server/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.api.server.repositories;
 import com.api.server.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
 }
