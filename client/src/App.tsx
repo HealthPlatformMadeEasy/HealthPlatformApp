2a2dfb2b819cd824e8749313f510aa11d939be03
Maximiliano Barrientos <elbodysathba@gmail.com>
2023-07-11 09:53:32 +0200

    summary feature at app.tsx
    Signed-off-by: Maximiliano Barrientos <elbodysathba@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
 import CalorieCalculator from "./components/CalorieCalculator/CalorieCalculator.tsx";
 
+const data = {"calories": 250}
+
 function App() {
+
+    const foodSummaryFeature = false;
 
     return (
         <>
             < CalorieCalculator/>
+            {foodSummaryFeature??
+            <div>
+                <pre>
+                    {JSON.stringify(data, null, 2)}
+                </pre>
+            </div>
+            }
         </>
     )
 }
