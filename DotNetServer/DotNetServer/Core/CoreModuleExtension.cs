1b956c098392a5c519411804a55ca67d1f439ef0
Maximiliano Barrientos <elbodysathba@gmail.com>
2023-08-21 21:40:36 +0200

    Add validation for missing connection string
    Updated the configuration accessor in CoreModuleExtension.cs for both Production and Development environments to throw an exception when the connection string is missing. This is to ensure program stability and catch configuration errors before they cause issues at
    
    Signed-off-by: Maximiliano Barrientos <elbodysathba@gmail.com>

--- DotNetServer/DotNetServer/Core/CoreModuleExtension.cs
+++ DotNetServer/DotNetServer/Core/CoreModuleExtension.cs
@@ -8,11 +8,12 @@ public static class CoreModuleExtension
     public static IServiceCollection AddCoreModuleLayer(this IServiceCollection services, WebApplicationBuilder builder)
     {
         services.AddDbContext<NorwegianFoodDbContext>(opt =>
-            opt.UseNpgsql(builder.Configuration.GetConnectionString("Production")));
+            opt.UseNpgsql(builder.Configuration["Production"] ??
+                          throw new InvalidOperationException("Connection string not found.")));
 
 
         // services.AddDbContext<NorwegianFoodDbContext>(opt =>
-        //     opt.UseNpgsql(builder.Configuration.GetConnectionString("Development")));
+        //     opt.UseNpgsql(builder.Configuration["Development"] ?? throw new InvalidOperationException("Connection string not found.")));
 
         return services;
     }
