b30ece16cf53b2ea2261b44c8ef22fd1474ab2ba
Maximiliano Barrientos <elbodysathba@gmail.com>
2023-08-31 18:35:19 +0200

    Remove unused DotNetServer/wwwroot/assets/index-3a382489.js file
    The client-side file "DotNetServer/wwwroot/assets/index-3a382489.js" has been removed because it is no longer used in the application. This include clean-up eliminates the need to load and maintain unnecessary JavaScript files, optimizing overall performance. Special attention has been paid to ensure that the removal of this file does not affect other parts of the application.
    
    Signed-off-by: Maximiliano Barrientos <elbodysathba@gmail.com>

--- client/src/pages/TestPage.tsx
+++ client/src/pages/TestPage.tsx
@@ -1,18 +1,3 @@
-import { Calendar } from "../components/Charts/Calendar.tsx";
-import { Tabs } from "../components/Calendar/TabTest.tsx";
-import { IGenericMacroDataChart } from "../Model";
-
-const priceData: IGenericMacroDataChart[] = [
-  { createdAt: new Date("2023-08-13"), value: 90 },
-  { createdAt: new Date("2023-08-14"), value: 150 },
-  { createdAt: new Date("2023-08-15"), value: 105 },
-];
-
 export function TestPage() {
-  return (
-    <div>
-      <Calendar priceData={priceData} goal={100} />
-      <Tabs />
-    </div>
-  );
+    return <div></div>;
 }
